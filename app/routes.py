50e7fd001f402fd515193098911d3bc08ccc0aa5
Mateusz <wasilewskimateusz1998@gmail.com>
2019-12-16 01:04:34 +0100

    Add requirements

--- app/routes.py
+++ app/routes.py
@@ -18,7 +18,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index;'))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
